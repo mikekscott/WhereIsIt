32fab135a255770bdefe08c45751dac3a1b426bf
mikekscott <mikekscott@gmail.com>
2019-05-23 10:05:33 +0100

    Corrected initial core data setup ( save context within funct application will terminate

--- WhereIsIt/Controller/AppDelegate.swift
+++ WhereIsIt/Controller/AppDelegate.swift
@@ -40,6 +40,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        //code added due to use of coredata
+        self.saveContext()
     }
 
     // MARK: - Core Data stack
